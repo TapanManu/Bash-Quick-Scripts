2a01fb91d6a1c87d9ca849387754495398b7fc09
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-10 14:10:49 +0530

    Create commandline.sh

---
+++ shell_script/commandline.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Name of your script is $0"
+echo "arguments entered on command line $*"
+echo "first argument: $1"
+echo "second argument: $2"
+echo "you entered  $# arguments"
+echo "process id is $$"
