5d082ac40ebc6b5a78551c9fb76b7e87fc696ec0
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-02-16 21:34:11 +0530

    Create days.sh

---
+++ shell_script/basic/days.sh
@@ -0,0 +1,10 @@
+read -p "get the day number" day
+case $day in
+1)echo "sunday" ;;
+2)echo "monday" ;;
+3)echo "tuesday" ;;
+4)echo "wednesday" ;;
+5)echo "thursday" ;;
+6)echo "friday" ;;
+7)echo "saturday" ;;
+esac
